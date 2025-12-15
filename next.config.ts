bef8659d7f0fc074fa0b1e4292b41da037bb1192
tkhumush <tkhumush@outlook.com>
2025-12-14 22:17:31 -0500

    Configure static export for root deployment

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,9 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   output: 'export',
-  basePath: isProd ? '/Resume' : undefined,
-  assetPrefix: isProd ? '/Resume/' : undefined,
   images: {
     unoptimized: true,
   },
