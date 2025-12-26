64db5cdab119d96051451c177ef9b9a6fd475a29
tkhumush <tkhumush@outlook.com>
2025-12-26 00:05:59 -0500

    Remove docs folder and export script - deploying from root

--- docs/_next/static/chunks/5fe77aa33bd6eca6.js
+++
@@ -1,8 +0,0 @@
-__turbopack_load_page_chunks__("/contact", [
-  "static/chunks/c0ced4c0818a8696.js",
-  "static/chunks/068c53b3359fcc88.js",
-  "static/chunks/84ea5d582b6b4276.js",
-  "static/chunks/15fec500af9afd5f.js",
-  "static/chunks/db2bfe74d307fa57.js",
-  "static/chunks/turbopack-3dbd9035bbc9cad1.js"
-])
