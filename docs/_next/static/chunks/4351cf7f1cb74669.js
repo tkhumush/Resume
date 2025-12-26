311c741f95a8e552772194d38d2fa5386771ecda
tkhumush <tkhumush@outlook.com>
2025-12-25 23:42:59 -0500

    Build latest version for deployment

--- docs/_next/static/chunks/4351cf7f1cb74669.js
+++
@@ -1,7 +0,0 @@
-__turbopack_load_page_chunks__("/", [
-  "static/chunks/7c8e5b769bbc2d21.js",
-  "static/chunks/079f81d068dcffa8.js",
-  "static/chunks/4d6f7fb1bd3ee7a2.js",
-  "static/chunks/f9b7479eb7ab4e1b.js",
-  "static/chunks/turbopack-aa28f2872215a1da.js"
-])
--- docs/_next/static/chunks/5556f50c590017ae.js
+++
@@ -1,8 +0,0 @@
-__turbopack_load_page_chunks__("/contact", [
-  "static/chunks/c0ced4c0818a8696.js",
-  "static/chunks/f8976357901d4bf1.js",
-  "static/chunks/004a9371068e267b.js",
-  "static/chunks/f9b7479eb7ab4e1b.js",
-  "static/chunks/4d6f7fb1bd3ee7a2.js",
-  "static/chunks/turbopack-d871d9517e508e33.js"
-])
